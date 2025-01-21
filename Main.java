b20a4d5b7fcbad55eddf858275c9ced449061977
jinx5675 <ms.prity3782@gmail.com>
2025-01-21 11:10:28 +0600

    Added syntax error

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String [] args){
         int num1 =10;
-        int num2=30;
+        int num2="30";
         System.out.println(num1+num2);
-        
     }
-    
 }
