f2d00cb9c08aab3f10c8ed4cc6ca7033f8ac6e40
jinx5675 <ms.prity3782@gmail.com>
2025-01-21 11:32:55 +0600

    Added comment for readability

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    // TODO: Add comments in the code describing what you are doing'
+    // TODO: create new commit with message 'Added comments for readability'
+    // TODO: Push
     public static void main(String [] args){
         int num1 =10;
         int num2=30;
